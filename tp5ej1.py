4d7b6d27b8703bdeb7eb85b12ce7bbb253f935fa
jean franco <favreaujeanfranco@gmail.com>
2021-06-01 21:04:08 -0300

    Ejercicio 1

---
+++ tp5ej1.py
@@ -0,0 +1,22 @@
+################
+# Jean Favreau - @JeanFranco99
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def es_par_o_impar(numero):
+    """Funcion para evaluar si un numero es par o impar"""
+    while numero > 0:
+        numero = numero - 2
+    if numero == 0:
+        return True
+    else:
+        return False
+
+def prueba():
+    num = int(input("Ingrese el numero a evaluar: "))
+    evaluar = es_par_o_impar(num)
+    print("El numero es par? ", evaluar)
+    
+if __name__ == "__main__":
+    prueba()
